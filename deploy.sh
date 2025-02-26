4f627cc3290f47c68eb29ab9092d000cd53281b9
ogerly <ogerly@tutanota.com>
2025-02-26 15:59:35 +0100

    change deploy

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+# Abbrechen bei Fehlern
+set -e
+
+# Navigiere in das Projektverzeichnis
+cd "DEVmatrose - Company"
+
+# Build erstellen
+npm run build
+
+# In das Build-Verzeichnis wechseln
+cd ../dist
+
+# CNAME-Datei erstellen, falls eine benutzerdefinierte Domain verwendet wird
+# echo 'www.devmatrose.de' > CNAME
+
+# .nojekyll-Datei erstellen, um die Jekyll-Verarbeitung auf GitHub Pages zu verhindern
+touch .nojekyll
+
+# Git initialisieren, wenn es noch nicht existiert
+if [ ! -d ".git" ]; then
+  git init
+fi
+
+# Git konfigurieren
+git checkout -B gh-pages
+git add -A
+git commit -m 'Deploy auf GitHub Pages'
+
+# Push zu GitHub Pages-Branch
+# Stelle sicher, dass du Zugriff auf das Repository hast
+git push -f git@github.com:ogerly/DEVmatrose-website2.git gh-pages:gh-pages
+
+# Zurück zum ursprünglichen Verzeichnis
+cd -
+
+echo "Deployment abgeschlossen! Überprüfe deine Seite unter https://ogerly.github.io/DEVmatrose-website2/"
